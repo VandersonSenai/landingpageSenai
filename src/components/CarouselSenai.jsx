d3daa428ab42d0b4760fb85bd1684b6d1823ffb8
VandersonSenai <vanderson.silva13@aluno.senai.br>
2025-05-19 13:27:30 -0300

    Renomeando arquivos a tratando imagem do carousel.

--- src/components/CarouselSenai.jsx
+++ src/components/CarouselSenai.jsx
@@ -18,6 +18,24 @@ function CarouselSenai() {
                   className='mb-5 mt-3' 
                   indicators={false}>	
           <Carousel.Item>
+            <a href="https://loja.senaies.com.br/" target="_blank" rel="noopener noreferrer">
+            <img
+              className="d-block "
+              src="/carousel_imagem_02.png"
+              alt="Curos Senai"
+            />
+            </a>
+          </Carousel.Item>    
+                    <Carousel.Item>
+            <Link to="/historia">
+              <img
+                className="d-block"
+                src="/carousel_imagem_03.jpg"
+                alt="Historia do Senai"
+              />
+            </Link>
+          </Carousel.Item>         
+          <Carousel.Item>
               <a href="https://conteudo.senaies.com.br/cursos-qualificacao" 
                   target="_blank" 
                   rel="noopener noreferrer">
@@ -27,26 +45,7 @@ function CarouselSenai() {
               alt="Acelera-Senai"
             />
             </a>
-
-          </Carousel.Item>
-          <Carousel.Item>
-            <a href="https://loja.senaies.com.br/" target="_blank" rel="noopener noreferrer">
-            <img
-              className="d-block "
-              src="/carousel_imagem_02.png"
-              alt="Curos Senai"
-            />
-            </a>
-          </Carousel.Item>          
-          <Carousel.Item>
-            <Link to="/historia">
-              <img
-                className="d-block"
-                src="/carousel_imagem_03.JPG"
-                alt="Historia do Senai"
-              />
-            </Link>
-          </Carousel.Item>
+         </Carousel.Item>
         </Carousel>
       </Col>
     </Row>
